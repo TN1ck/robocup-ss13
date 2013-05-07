1237aaf51788daf6ed2124017d26d844eacd18c7
Tom Nick <Tom.w.a.nick@gmail.com>
2013-05-07 16:37:05 +0200

    Whitespace and pep8 fixes

--- src/movement.py
+++ src/movement.py
@@ -3,26 +3,23 @@ from math import sin, cos
 
 class Movement:
 
-	def __init__(self, world, socket):
-		self.world = world
-		self.socket = socket
-		self.velocity = 0;
-		self.stepsize = 0.1
+    def __init__(self, world, socket):
+        self.world = world
+        self.socket = socket
+        self.velocity = 0
+        self.stepsize = 0.1
 
+    def run(self, velocity):
+        self.velocity = velocity
+        player = self.world.get_player
+        dy = sin(player.rot) * self.stepsize * self.velocity
+        dx = cos(player.rot) * self.stepsize * self.velocity
+        self.socket.send(" ".join(map(str, ["(beam",
+                                            player.pos.x + dx,
+                                            player.pos.y + dy, player.rot, ")"]
+                                      )))
+        self.world.player.pos.x = player.pos.x + dx
+        self.world.player.pos.y = player.pos.y + dy
 
-	def run(self, velocity):
-		self.velocity = velocity
-		player = self.world.get-player
-		dy = sin(player.rot) * self.stepsize * self.velocity
-		dx = cos(player.rot) * self.stepsize * self.velocity
-		self.socket.send(" ".join(map (str, ["(beam", player.pos.x + dx, player.pos.y + dy, player.rot, ")"])))
-		self.world.player.pos.x = player.pos.x + dx
-		self.world.player.pos.y = player.pos.y + dy 
-
-
-	def update(self):
-		self.run(self, self.velocity)
-
-
-
-
+    def update(self):
+        self.run(self, self.velocity)
