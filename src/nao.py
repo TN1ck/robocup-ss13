6b54b1ecabad52d4289210f88d469f4531eec013
Nadine <nadine.r@mailbox.tu-berlin.de>
2013-05-27 18:01:34 +0200

    add comments in agent and nao from keyframe group

--- src/nao.py
+++ src/nao.py
@@ -16,11 +16,6 @@ class Nao:
     def __init__(self, world, player_nr):
         self.world = world
         self.player_nr = player_nr
-        name = ['Time', 'Neck Pitch', 'Neck Yaw', 'RShoulderPitch', 'Left Shoulder Pitch', 
-                'RShoulderRoll', 
-'Left Shoulder Yaw' ,'RElbowRoll', 'Left Arm Roll', 'RElbowYaw', 'Left Arm Yaw', 'RHipYawPitch', 
-'Left Hip YawPitch', 'RHipPitch', 'Left Hip Pitch', 'RHipRoll', 'Left Hip Roll', 'RKneePitch', 'LKneePitch', 
-'RAnklePitch', 'LAnklePitch', 'RAnkleRoll', 'LAnkleRoll']
         self.hinge_joints = []
         self.hinge_joints += [
             HingeJoint('Neck Yaw', 'hj1', 'he1', -120, 120), 
@@ -63,6 +58,9 @@ class Nao:
         return self.world.entity_from_identifier['P' + str(self.player_nr)].get_see_vector()
     
     def update_joint_positions(self, parsed):
+        '''
+        update all currently perceived joints of the nao
+        '''
         joint_number = 0
         for i in parsed:
             if i[0] == 'HJ':
