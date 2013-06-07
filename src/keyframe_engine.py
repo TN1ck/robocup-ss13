4af611a5fb6c1bd54a2bd2b1e01897c7f8579f7a
lukas-str <lukas@LukasLaptopUbuntu.(none)>
2013-06-07 23:26:49 +0200

    First version off keyframe engine for headmovement while other keyframes run Not yet finished! Uploaded so we can explain the correct functions in the presentation

--- src/keyframe_engine.py
+++ src/keyframe_engine.py
@@ -16,101 +16,112 @@ class Keyframe_Engine:
         self.working = False    # For tactics, if TRUE start keep_going
         self.last = 0
         self.last_frame = None  # Last keyframe
+        self.head_working = False
+        self.head_frame = None
+        self.head_line = 0
+        self.head_time = 0
+        self.head_last = 0
+        self.head_stop = False
 
         
-    def keep_going(self):
+    def work(self):
         '''
-        Fuction to keep the last keyframe running
+        Runs the actuall keyframes, has to be called at end of tactics!
         '''
         if self.last_frame is not None:
             keyframe = self.last_frame.keyframe
             name = self.last_frame.name
             self.next_step(keyframe, name)
+        
+        if self.head_frame is not None:
+            keyframe = self.head_frame.keyframe
+            name = self.head_frame.name
+            self.next_head_step(keyframe, name)
     
     def fall_on_front(self):
         '''
         Testfunction to let the Nao fall on its front
         '''
-        keyframe = kf.fall_front.keyframe
-        name = kf.fall_front.name
-        self.fall = True
         self.last_frame = kf.fall_front
+        self.fall = True
         self.working = True
-        self.next_step(keyframe, name)
     
     def fall_on_back(self):
         '''
         Testfunction to let the Nao fall on its back
         '''
-        keyframe = kf.fall_back.keyframe
-        name = kf.fall_back.name
-        self.working = True
-        self.fall = True
         self.last_frame = kf.fall_back
-        self.next_step(keyframe, name)
+        self.fall = True
+        self.working = True
     
     def stand(self):
         '''
         Stand position of the NAO
         '''
-        keyframe = kf.stand.keyframe
-        name = kf.stand.name
         self.last_frame = kf.stand
         self.working = True
-        self.next_step(keyframe, name)
     
     def stand_up_from_back(self):
         '''
         Stand_up-function from back
         '''
-        keyframe = kf.stand_up_from_back.keyframe
-        name = kf.stand_up_from_back.name
+        self.last_frame = kf.stand_up_from_back
         self.working = True
         self.fall = False
-        self.last_frame = kf.stand_up_from_back
-        self.next_step(keyframe, name)
                
     def stand_up_from_front(self):
         '''
         Stand_up-function from front
         '''
-        keyframe = kf.stand_up_from_front.keyframe
-        name = kf.stand_up_from_front.name
         self.last_frame = kf.stand_up_from_front
         self.working = True
         self.fall = False
-        self.next_step(keyframe, name)
         
-    def lookAround(self):
+    def head_lookAround(self):
         '''
         funktion for look around
         '''
-        keyframe = kf.lookAround.keyframe
-        name = kf.lookAround.name
-        self.last_frame = kf.lookAround
-        self.working = True
-        self.next_step(keyframe, name)
+        self.head_frame = kf.lookAround
+        self.head_working = True
         
     def kick1(self):
         '''
         first kick-function
         the ball has to lay in front of the nao
         '''
-        keyframe = kf.kick1.keyframe
-        name = kf.kick1.name
         self.last_frame = kf.kick1
         self.working = True
-        self.next_step(keyframe, name)
     
     def test_frame(self):
         '''
         Testfunction that moves all  joints
         '''
-        keyframe = kf.testframe.keyframe
-        name = kf.testframe.name
         self.last_frame = kf.testframe
         self.working = True
-        self.next_step(keyframe, name)
+    
+    def head_move(self, angle):
+        '''
+        Function to move the head horizontal to the given angle. Not yet finished.
+    
+        self.head_frame = kf.head_reset
+        self.head_frame.keyframe = [[math.fabs(angle)*3, 0.0, angle]]
+        self.head_working = True
+        '''
+        
+    def head_stop(self):
+        '''
+        Stops the head movement at the actual position. Not yet finished.
+        
+        self.head_stop = True
+        '''
+        
+    def head_reset(self):
+        '''
+        Resets the head to 0-Position. Not yet finished.
+        
+        self.head_frame = kf.head_reset
+        self.head_working = True
+        '''
             
     def next_step(self, keyframe, name):
         '''
@@ -122,8 +133,13 @@ class Keyframe_Engine:
         if self.last == 1:
             #the keyframe is finished and does not repeat
             #perhaps important for the communication with the tactics group
-            self.last_joint_speed = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-            self.last_joint_angle = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+            i = 0
+            if self.head_working:
+                i = 2
+            while i < len(self.last_joint_speed):
+                self.last_joint_speed[i] = 0.0
+                self.last_joint_angle[i] = 0.0
+                i = i + 1
             self.last_frame = None
             self.working = False
             self.last = 2
@@ -134,17 +150,70 @@ class Keyframe_Engine:
                 self.keyframe_line = 0
                 self.last = 1
         i = 0
+        if self.head_working:
+            i = 2
         while i < len(self.nao.hinge_joints):
             self.send(self.nao.hinge_joints[i].effector, self.last_joint_speed[i])
             i = i + 1
         if self.last == 2:
             self.last = 0        
+            
+    def next_head_step(self, keyframe, name):
+        '''
+        calculates the next head joints speed and sends them to the socket
+        '''
+        if self.head_last == 1:
+            self.last_joint_speed[0] = 0.0
+            self.last_joint_speed[1] = 0.0
+            self.last_joint_angle[0] = 0.0
+            self.last_joint_angle[1] = 0.0
+            if self.head_frame is not kf.lookAround:
+                self.head_frame = None
+                self.head_working = False
+            self.head_last = 2
+            
+        if self.head_last == 0:
+            self.get_new_head_position(keyframe[self.head_line], name)
+            if self.head_line >= len(keyframe):
+                self.head_line = 0
+                self.head_last = 1
+        self.send(self.nao.hinge_joints[0].effector, self.last_joint_speed[0])
+        self.send(self.nao.hinge_joints[1].effector, self.last_joint_speed[1])
+        if self.head_last == 2:
+            self.head_last = 0
+            
+    def get_new_head_position(self, keyframe, name):
+        '''
+        Calculate the new head speed in rad/sec
+        '''
+        joint_number = 0
+        while joint_number < 2:
+            joint_name = 1
+            while joint_name < len(name):
+                if name[joint_name] == self.nao.hinge_joints[joint_number].description:
+                    self.angle = keyframe[joint_name] - (self.nao.hinge_joints[joint_number].value + self.last_joint_angle[joint_number])
+                    if(self.nao.hinge_joints[joint_number].min > keyframe[joint_name]):
+                        self.angle = self.nao.hinge_joints[joint_number].min
+                    if(self.nao.hinge_joints[joint_number].max < keyframe[joint_name]):
+                        self.angle = self.nao.hinge_joints[joint_number].max
+                    self.angle = self._default_time * self.angle / (keyframe[0]-self.head_time)
+                    self.last_joint_angle[joint_number] = self.angle
+                    self.last_joint_speed[joint_number] = (math.radians(self.angle)/(self._default_time*0.001))
+                    joint_number = joint_number + 1
+                    break
+                joint_name = joint_name + 1
+        self.head_time = self.head_time + 20
+        if (keyframe[0] - self.head_time) < self._default_time:                
+            self.head_time = 0
+            self.head_line = self.head_line + 1
     
     def get_new_joint_postion(self, keyframe, name):
         '''    
         Function to calculate the new speed in rad/sec
         '''
         joint_number = 0
+        if self.head_working:
+            joint_number = 2
         while joint_number < len(self.last_joint_speed):
             joint_name = 1
             while joint_name < len(name):
