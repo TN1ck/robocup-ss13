a3cc7426bf85cb2f449496c647b8dc9352c5329c
teambo <tim.classen@gmx.de>
2013-06-10 15:26:39 +0200

    the agent for the presentation. is the older agent with the working keyframes but has a few bugs to be fixed at the moment (crashes when he should fall on back)

---
+++ src/testagent.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sock
+import parser
+import world
+import perception
+import logging
+import tactics_main
+import movement
+import nao
+import keyframe_engine
+from sys import argv
+
+global our_team
+our_team = "DAI-Labor"
+
+
+
+class Agent:
+    def __init__(self, p_nr):
+        self.player_nr = p_nr #TODO needs to be revised for multiple players
+
+        self.world = world.World(6, 30, 20) # 6 players per team, field size: 30 meters x 20 meters
+
+        self.nao = nao.Nao(self.world, self.player_nr)
+
+        self.perception = perception.Perception(self.player_nr, our_team)
+
+        #setup a agentSocket-connection to the server
+        self.agentSocket = sock.Sock("localhost", 3100, our_team, self.player_nr)
+        self.monitorSocket = sock.Sock("localhost", 3200, None, None)
+        self.start()
+
+    def start(self):
+        #start listen#self.perception.process_vision_perceptors(parsed_stuff, self.world)ing
+        self.agentSocket.start()
+        self.monitorSocket.start()
+
+
+        #m = movement.Movement(self.world, self.monitorSocket, self.player_nr, our_team)
+
+        m = movement.Movement(self.world, self.agentSocket, self.player_nr)
+        kfe = keyframe_engine.Keyframe_Engine(self.nao, self.agentSocket)
+        t = tactics_main.TacticsMain(self.world, m, self.nao)
+
+        # You need a first flush in order that "beam" works
+        self.agentSocket.flush()
+
+        #msg = self.agentSocket.receive()
+        #parsed_stuff = parser.parse_sexp(msg)
+        #self.perception.process_vision(parsed_stuff, self.world)
+        #self.perception.process_gyros(parsed_stuff, self.nao)
+
+        offset_for_player = -9 + (3*self.player_nr)
+
+        self.agentSocket.enqueue(" ( beam -10 "+ str(offset_for_player) +" 0 ) ")
+        #self.agentSocket.enqueue(" ( beam -1 -1 180 ) ")
+        self.agentSocket.flush()
+ 
+        i = 1
+        while True:
+            msg = self.agentSocket.receive()
+
+            #logging.debug(msg)
+            parsed_stuff = parser.parse_sexp(msg)
+
+            self.perception.process_joint_positions(parsed_stuff, self.nao)
+            self.perception.process_vision(parsed_stuff, self.world)
+            self.perception.process_gyros(parsed_stuff, self.nao)
+
+            #logging.debug('gyro rate: ' + str(self.nao._gyro_rate))
+            #logging.debug('gyro state: ' + str(self.nao.get_gyro_state()))
+
+            #lets the nao stand up from back
+            # self.agentSocket.enqueue("( beam 0 0 0 )")
+
+            #logging.debug('agent location: ' + str(self.world.get_entity_position('P' + str(self.player_nr))))
+            #logging.debug('agent location: ' + str(self.nao.get_position()))
+            #logging.debug('agent see vector: ' + str(self.nao.get_see_vector()))
+            #logging.debug('ball pos: ' + str(self.world.entity_from_identifier['B'].get_position()))
+            
+            if i > 10 and i < 600 :
+                m.run(0, 0)
+            if i > 600 and i < 900:
+                kfe.fall_on_front()
+                #m.run(-10, 9)
+            if i > 900 and i < 1100:
+                kfe.stand_up_from_front()
+            if i > 1150 and i < 1250:
+                m.run(0, 0)
+            if i > 1250 and i < 1450:
+                kfe.fall_on_back()
+            if i > 1500 and i < 1700:
+                kfe.stand_up_from_back()
+            if i > 1700 and i < 1800:
+                m.run(0, 0)
+            if i > 1850 and i < 1950:
+                kfe.kick1()
+                #m.run(0, 0)
+            
+            i = i + 1
+            print i
+            kfe.work()
+            # t.run_tactics()
+            self.agentSocket.flush()
+
+            # logging.debug(world.w.flags[0].get_position().x)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    try:
+        agent_id = int(argv[1])
+    except:
+        print("You need to call \"./agent.py <player_id>\".")
+        exit(1)
+
+    Agent(agent_id)
