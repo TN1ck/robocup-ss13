2341b14c33ad9356876e658c8e0426d2a409ddc6
Nadine <nadine.r@mailbox.tu-berlin.de>
2013-06-17 19:35:09 +0200

    add new kick1, kick2 is not in use

---
+++ src/Keyframes/kick2.py
@@ -0,0 +1,11 @@
+name = ['Time', 'Neck Pitch', 'Neck Yaw', 'Right Shoulder Pitch', 'Left Shoulder Pitch', 
+        'Right Shoulder Yaw', 'Left Shoulder Yaw' ,'Right Arm Roll', 'Left Arm Roll', 
+        'Right Arm Yaw', 'Left Arm Yaw', 'Right Hip YawPitch', 'Left Hip YawPitch', 'Right Hip Pitch',
+        'Left Hip Pitch', 'Right Hip Roll', 'Left Hip Roll', 'Right Knee Pitch', 'Left Knee Pitch', 
+        'Right Foot Pitch', 'Left Foot Pitch', 'Right Foot Roll', 'Left Foot Roll']
+
+keyframe =[[800, 0, 0, -120.0, -120.0, 0.0, 0.0, 90.0, -90.0, 90.0, -90.0, 0, 0, 0, 0, 0, 0, -25, 0, 16, 0, 0, 0],
+           [800, 0, 0, -120.0, -120.0, 0.0, 0.0, 90.0, -90.0, 90.0, -90.0, 0, 0, 20, 0, 0, 0, 0, 0, -5, 0, 0, 0],
+           [800, 0, 0, -120.0, -120.0, 0.0, 0.0, 90.0, -90.0, 90.0, -90.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
