3a33d2ea777431e70f3c6928cb91a032b7d758b1
leonard <cyborgx7@gmail.com>
2013-06-22 22:31:30 +0200

    run_to_shoot_position() added to movement.py

--- src/movement.py
+++ src/movement.py
@@ -7,13 +7,15 @@ class Movement:
         self.player_nr = player_nr
         #self.team = team
         self.velocity = 0.02
-        self.divergence = 0.1
+        self.divergence = 0.27
         self.stopped = True
         self.destination = None
-        self.angular_precision = 0.5;
+        self.angular_precision = 0.1;
         self.rotation = 0
         self.position = self.world.entity_from_identifier['P_1_' + str(self.player_nr)].get_position()
         self.beampos = self.position
+        self.shoot_distance = hypot(-0.2, 0.05)
+        self.shoot_angle_offset = atan2(-0.2, 0.05)
         self.fresh = True
 
     def get_world(self):
@@ -23,8 +25,8 @@ class Movement:
         self.socket.enqueue(" ".join(map(str, ["("] + list(params) + [")"])))
 
     def run(self, *destination):
-        #print destination[0]
-        #print destination[1]
+        print destination[0]
+        print destination[1]
         if self.fresh:
             self.beampos = self.world.get_entity_position('P_1_' + str(self.player_nr))
             self.fresh = False
@@ -64,6 +66,23 @@ class Movement:
         #self.socket.receive()
         #self.send("beam", self.beampos.x, self.beampos.y, (-1 * degrees(self.rotation) )+90)
 
+    def turn(self, angle):
+        if self.fresh:
+            self.beampos = self.world.get_entity_position('P_1_' + str(self.player_nr))
+            self.fresh = False
+        self.send("agent (unum", self.player_nr, ") (team Left) (move", self.beampos.x, self.beampos.y, "0.38", angle, ")")
+        
+    def run_to_shoot_position(self, *destination):
+        self.position = self.world.get_entity_position('P_1_' + str(self.player_nr))
+        ballposition = self.world.ball.get_position()
+        ballposition.x = 0
+        ballposition.y = 0
+        a = atan2(destination[0] - ballposition.x, destination[1] - ballposition.y)
+        if (hypot(ballposition.x - self.position.x, ballposition.y - self.position.y) > self.divergence):
+            self.run(ballposition.x + (sin(a + self.shoot_angle_offset) * self.shoot_distance), ballposition.y + (cos(a + self.shoot_angle_offset) * self.shoot_distance))
+        else:
+            self.turn(degrees(a) - 80)
+        return
 
     def stop(self):
         self.stopped = True
