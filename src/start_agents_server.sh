81b1c223cdf0c26ebf53ddb7f0b1b32a0a9ff1b0
Jacob Kauffmann <jackmcrider@gmail.com>
2013-06-24 18:42:24 +0200

    You need to add your roboviz.sh to a the $PATH. Add '''PATH=$PATH:/path/to/your/roboviz''' to your ~/.profile

---
+++ src/start_agents_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rcssserver3d &
+
+if command -v roboviz.sh >/dev/null 2>&1; then
+  roboviz.sh &
+else
+  rcsoccersim3d &
+fi
+
+for i in `seq 1 11`; do
+  # Executes agents asynchronously
+  ./agent.py $i &
+  # Print process id of every agent in order that you can kill them
+  echo $! >> .kill_info
+  sleep 2s
+done
