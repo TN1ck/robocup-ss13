c120086854302251f4b36be185ff0e0ab762133b
helme <helme@posteo.de>
2013-06-25 16:29:48 +0200

    changed number of agents back to 6

--- src/start_agents.sh
+++ src/start_agents.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq 1 11`; do
+for i in `seq 1 6`; do
   # Executes agents asynchronously
   ./agent.py $i &
   # Print process id of every agent in order that you can kill them
