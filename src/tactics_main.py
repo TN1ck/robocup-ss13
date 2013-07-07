ea0a0937ec789bbfa884eb0feb5f6792204583dc
MatBook <MatthiasCK@example.com>
2013-07-07 19:17:43 +0200

    some tactics changes

--- src/tactics_main.py
+++ src/tactics_main.py
@@ -22,8 +22,6 @@ class TacticsMain:
     self.my_position = None
 
     self.field_lines_idfs   = ["L1","L2","LL","LR","LM"]       #Field lines
-    # self.right_penalty_idfs = ["LG1R","LG2R","LGR"]            #right penalty area
-    # self.left_penalty_ifds  = ["LG1L","LG2L","LGL"]            #left penalty area
     self.right_goal_idfs    = ["G1R","G2R"]                    #right goal
     self.left_goal_idfs     = ["G1L","G2R"]                    #left goal
     self.player_t1_idfs     = ["P_1_1","P_1_2","P_1_3","P_1_4","P_1_5","P_1_6","P_1_7","P_1_8","P_1_9","P_1_10","P_1_11"]  #team 1
@@ -39,10 +37,10 @@ class TacticsMain:
     self.headAngle = 0
     self.run_straight =False
     self.dest = None
-    self.ball_owner = False
     self.ball_search_mode = 0
     self.turn_counter = 0
-    self.offence = False
+    self.offence_member = False
+    self.com_counter = 0
 
 
   def set_own_position(self):
@@ -57,12 +55,6 @@ class TacticsMain:
         if self.world.entity_from_identifier[self.player_t1_idfs[i]].confidence > 0.5:
           val = self.calc_point_distance(self.world.get_entity_position(self.player_t1_idfs[i]), self.my_position)
           self.distance_team1.append((self.player_t1_idfs[i], val))
-
-      # for i in range(len(self.player_t2_idfs)):
-      #   try:
-      #     if self.world.entity_from_identifier[self.player_t2_idfs[i]].confidence > 0.5:
-      #       val = self.calc_point_distance(self.world.get_entity_position(self.player_t2_idfs[i]), self.my_position)
-      #       self.distance_team2.append((self.player_t2_idfs[i], val))
 
       self.calculate_goal_distances(self.my_position)
       self.distance_lines = self.calc_line_distance(self.my_position)
@@ -178,16 +170,25 @@ class TacticsMain:
 
       new_tuples.append((self.my_position.x + x_dist,
                    self.my_position.y + y_dist))
-
     average_list = [0, 0]
     for i in new_tuples:
       average_list[0] += i[0]
       average_list[1] += i[1]
-
     return (average_list[0]/len(too_near), average_list[1]/len(too_near))
 
+
+  def calc_turn_angle(self):
+    ball = self.world.get_entity_position('B')
+    ball.x -= self.my_position.x
+    ball.y -= self.my_position.y
+    turn = atan2(ball.y,ball.x) 
+    if turn < 0:
+      turn += 3.14   
+    return turn
+
+
   def search_ball(self):
-    if self.ball_owner:
+    if self.offence_member:
       if self.ball_search_mode == 0:
         self.run_straight = True
         self.dest= Vector(self.my_position.x - 0.3,self.my_position.y - 0.3)
@@ -198,37 +199,24 @@ class TacticsMain:
           self.turn_counter = 0
           self.ball_search_mode = 0
         self.turn_counter += 1
-        ball = self.world.get_entity_position('B')
-        ball.x -= self.my_position.x
-        ball.y -= self.my_position.y
-        turn = atan2(ball.y,ball.x)
-        if turn < 0:
-          turn += 3.14
+        turn = self.calc_turn_angle()
         return (('run', 'turn',turn),('stand_up',False),('kick',False),('say',False), ('head',False))
-      elif self.offence:
-        vec = self.flocking_behavior()
-        if vec is False:
-          return (('run', False),('stand_up',False),('kick',False),('say',False), ('head',True))
-        else:
-          return (('run', vec[0],vec[1]),('stand_up',False),('kick',False),('say',False), ('head',False))  
 
-  
-  def run_tactics(self,hearObj):
-    say_tuple = ('say',False)
-    if self.nao.lies_on_front():
-      return (('run', False),('stand_up','front'),('kick',False),say_tuple, ('head',True))
-    if self.nao.lies_on_back():
-      return (('run', False),('stand_up','back'),('kick',False),say_tuple, ('head',True))
 
-    self.clear_distances()
-    self.set_own_position()
-    if self.my_position == None:
-      return (('run', False),('stand_up',False),('kick',False),say_tuple, ('head',True))
+  def ball_info(self):
+    ball =  self.world.entity_from_identifier['B']
+    if self.com_counter == 0:
+      if ball.confidence == 1:
+        self.com_counter += 1
+        return ('say',5, 0,0,ball.get_position().x,ball.get_position().y)
+    else:
+      if self.com_counter == 50:
+        self.com_counter = 0
+      else:
+        self.com_counter += 1
+    return ('say',False)
 
-    if isinstance(hearObj, hearObject.BallPosition):
-      pass
-    self.get_distances()
-
+  def check_lines(self):
     for line in self.field_lines_idfs:
       if line == 'M':
         continue
@@ -246,18 +234,53 @@ class TacticsMain:
           self.run_straight = True
           self.dest = Vector(self.my_position.x - 0.3, self.my_position.y)
 
+  def run_to_xy(self,say_tuple):
+    if self.my_position == None or self.dest == None:
+        print "MyPosition = " +  str(self.my_position) + " dist " +str(self.dest)
+    vec = Vector(0,0)
+    vec.x = self.my_position.x / self.dest.x
+    vec.y = self.my_position.y / self.dest.y
+    if vec.x <= 1.1 and vec.x >= 0.9 and vec.y <= 1.1 and vec.y >= 0.9:
+      self.dest = None
+      self.run_straight = False
+    else:
+      return (('run', self.dest.x,self.dest.y),('stand_up',False),('kick',False), say_tuple, ('head',False))
+
+
+  def run_tactics(self,hearObj):
+    say_tuple = ('say',False)
+    run_tuple = ('run', False)
+    kick_tuple = ('kick', False)
+
+
+    if self.nao.lies_on_front():
+      return (('run', False),('stand_up','front'),('kick',False),say_tuple, ('head',True))
+
+    if self.nao.lies_on_back():
+      return (('run', False),('stand_up','back'),('kick',False),say_tuple, ('head',True))
+
+    self.clear_distances()
+
+    self.set_own_position()
+    if self.my_position == None:
+      return (('run', False),('stand_up',False),('kick',False),say_tuple, ('head',True))
+
+    if isinstance(hearObj, hearObject.BallPosition):
+      b = self.world.entity_from_identifier['B']
+      if b.confidence < 0.5:
+        b.set_position(hearObj.hearObject.x,hearObj.hearObject.y)
+        b.confidence = 0.8
+    
+    self.get_distances()
+    
+    say_tuple = self.ball_info()
+    
+    self.check_lines()
+
 
     if self.run_straight:
-      if self.my_position == None or self.dest == None:
-        print "MyPosition = " +  str(self.my_position) + " dist " +str(self.dest)
-      vec = Vector(0,0)
-      vec.x = self.my_position.x / self.dest.x
-      vec.y = self.my_position.y / self.dest.y
-      if vec.x <= 1.1 and vec.x >= 0.9 and vec.y <= 1.1 and vec.y >= 0.9:
-        self.dest = None
-        self.run_straight = False
-      else:
-        return (('run', self.dest.x,self.dest.y),('stand_up',False),('kick',False),say_tuple, ('head',False))
+      return self.run_to_xy(say_tuple)
+      
 
     if self.distances_ball == []:
       return self.search_ball()
@@ -266,12 +289,8 @@ class TacticsMain:
     offence = self.offence_Player()
     defence = not ball and not offence
 
-    result_list = [ball,offence,defence]
-    run_tuple = ('run', False)
-    kick_tuple = ('kick', False)
-    if result_list[0]:
-      self.offence = False
-      self.ball_owner = True
+    if ball:
+      self.offence_member = True
       if self.distances_ball[0][1] <= 0.5:
         if self.mov.reached_position :
           self.mov.reached_position = False
@@ -281,16 +300,14 @@ class TacticsMain:
       else:
         tup = self.world.entity_from_identifier['B'].get_position()
         run_tuple = ('run',tup.x,tup.y)
-    elif result_list[1]:
-      self.offence = True
-      self.ball_owner = True
+    elif offence:
+      self.offence_member = True
       tup = self.flocking_behavior()
       if tup is False :
         tup = (self.world.get_entity_position(self.distances_ball[0][0]).x-1.3,self.world.get_entity_position(self.distances_ball[0][0]).y-1.3)
       else:
         self.dest = Vector(tup[0],tup[1])
       run_tuple = ('run',tup[0],tup[1])
-    elif result_list[2]:
-        self.offence = False
-        self.ball_owner = False
+    elif defence:
+        self.offence_member = False
     return (run_tuple, ('stand_up',False), kick_tuple, say_tuple, ('head',False))
